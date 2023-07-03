9398c8b3dbf7bcbffda164042b78254d8cc0f789
Raheemcodes <raheemscorporatemail@gmail.com>
2023-07-03 14:11:12 +0100

    set up config file for deployment & verification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import fs from 'fs';
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+
+const INFURA_API_KEY = fs.readFileSync('.infura').toString().trim();
+const ETHERSCAN_API_KEY = fs.readFileSync('.etherscan').toString().trim();
+const mnemonic = fs.readFileSync('.secret').toString().trim();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: {
+        mnemonic,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 10,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,6 +2,8 @@ import { ethers } from 'hardhat';
 
 (async () => {
   try {
+    const nft = await ethers.deployContract('NFT');
+    await nft.waitForDeployment();
   } catch (err) {
     console.error(err);
     process.exitCode = 1;
