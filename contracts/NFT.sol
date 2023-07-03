c634668536e7443c8758ec1265fea8bb0dbdc080
Raheemcodes <raheemscorporatemail@gmail.com>
2023-07-03 12:04:25 +0100

    restructure test

---
+++ contracts/NFT.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyTestToken is ERC721, AccessControl {
+    using Counters for Counters.Counter;
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("MyTestToken", "TKN") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return
+            "https://ipfs.io/ipfs/QmUAfW9VqWJnq8RD9ZSmuvVKvCXaFt7VFyr33GHLAk3nwZ/";
+    }
+
+    function safeMint(address to) public onlyRole(MINTER_ROLE) {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function grantMintRole(address account) public {
+        grantRole(MINTER_ROLE, account);
+    }
+
+    function revokeMintRole(address account) public {
+        revokeRole(MINTER_ROLE, account);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ scripts/deploy.ts
@@ -0,0 +1,9 @@
+import { ethers } from 'hardhat';
+
+(async () => {
+  try {
+  } catch (err) {
+    console.error(err);
+    process.exitCode = 1;
+  }
+})();
