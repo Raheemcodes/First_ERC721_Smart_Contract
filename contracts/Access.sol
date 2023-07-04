daad66d3948b694023d3bcc6db385506c52d4e21
Raheemcodes <raheemscorporatemail@gmail.com>
2023-07-04 13:23:37 +0100

    Add granting & revoking of role function

--- contracts/Access.sol
+++ contracts/Access.sol
@@ -7,9 +7,31 @@ contract Access {
         Minter
     }
 
+    modifier onlyRole(Role _role) {
+        if (_role == Role.Admin) {
+            require(
+                roles[msg.sender] == _role,
+                "Only ADMIN can perform this action"
+            );
+        } else {
+            require(
+                roles[msg.sender] == _role,
+                "Only MINTER can perform this action"
+            );
+        }
+        _;
+    }
+
     mapping(address => Role) roles;
 
-    function grantRole(Role _role, address _account) internal {
+    function grantRole(
+        Role _role,
+        address _account
+    ) internal onlyRole(Role.Admin) {
         roles[_account] = _role;
+    }
+
+    function revokeRole(address _account) internal onlyRole(Role.Admin) {
+        delete roles[_account];
     }
 }
